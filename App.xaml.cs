9970dd12bbb18ba14c88707798ced9a2de26e2c7
Leandro A. Paiva <suporte_desenvolvimento@outlook.com>
2023-05-04 23:25:46 -0300

    feat: fazer funcionar.

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace UpxAppEdu;
+﻿using UpxAppEdu.View;
+
+namespace UpxAppEdu;
 
 public partial class App : Application
 {
@@ -6,6 +8,6 @@ public partial class App : Application
 	{
 		InitializeComponent();
 
-		MainPage = new AppShell();
-	}
+        MainPage = new AppShell();
+    }
 }
