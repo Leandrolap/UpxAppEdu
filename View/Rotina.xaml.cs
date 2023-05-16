5edb7327bca480178c8033aec07a0f132676fee2
Leandro A. Paiva <suporte_desenvolvimento@outlook.com>
2023-05-15 22:42:48 -0300

    feat: Redesenhando o projeto

--- View/Rotina.xaml.cs
+++ View/Rotina.xaml.cs
@@ -9,30 +9,4 @@ public partial class Rotina : ContentPage
 	{
 		InitializeComponent();
 	}
-
-    protected override async void OnAppearing()
-    {
-        base.OnAppearing();
-        TodoItemDatabase database = await TodoItemDatabase.Instance;
-        //listView.ItemsSource = await database.GetItemsAsync();
-    }
-
-    async void OnItemAdded(object sender, EventArgs e)
-    {
-        await Navigation.PushAsync(new AddItem
-        {
-            BindingContext = new TodoItem()
-        });
-    }
-
-    async void OnListItemSelected(object sender, SelectedItemChangedEventArgs e)
-    {
-        if (e.SelectedItem != null)
-        {
-            await Navigation.PushAsync(new AddItem
-            {
-                BindingContext = e.SelectedItem as TodoItem
-            });
-        }
-    }
 }
